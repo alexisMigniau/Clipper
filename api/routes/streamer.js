68a715704bcd26043b85472f5457281782c99dcb
Alexis <migniau.alexis@gmail.com>
2021-12-29 21:50:05 +0100

    Route search streamers

--- api/routes/streamer.js
+++ api/routes/streamer.js
@@ -1,8 +1,11 @@
+const twitch = require("../services/twitch");
+
 module.exports = {
     path: "/streamer",
     config : (router) => {
-        router.get("/search", async (req, res, next) => {
-            res.status(200).send("VIDE")
+        router.get("/search/:query", async (req, res, next) => {
+            const streamers = await twitch.searchUser(req.params.query)
+            res.status(200).send(streamers)
         });
 
         return router
---
+++ api/services/twitch.js
@@ -0,0 +1,15 @@
+const TwitchApi = require("node-twitch").default; 
+ 
+const twitch = new TwitchApi({ 
+	client_id: "9qs83g9lk7ff3i5l5r2r0feupgq3ab", 
+	client_secret: "j467j5wrzl0m66rrq2sr6myavkz0ez" 
+}); 
+ 
+module.exports.searchUser = async function(name) { 
+    const result = await twitch.searchChannels({query : name}) 
+    return result; 
+} 
+ 
+module.exports.getClips = async function(id){ 
+    return await twitch.getClips({broadcaster_id : id}) 
+} 
